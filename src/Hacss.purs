a22061ad83a96ed6c68d3515cb895010a8432269
Nick Saunders <nick@saunde.rs>
2020-10-30 20:11:51 -0700

    Remove extra line.

--- src/Hacss.purs
+++ src/Hacss.purs
@@ -1,4 +1,3 @@
---hello
 module Hacss (hacss) where
 
 import Prelude
@@ -13,28 +12,34 @@ import Foreign.Object (Object)
 import Hacss.Data (AtScope(..), Property(..), Variable(..))
 import Hacss.CSS (CSS, mkCSS)
 
-type Code = String
+type Code
+  = String
 
-type Config =
-  { atScopes :: Nullable (Object String)
-  , knownProperties :: Nullable (Array String)
-  , variables :: Nullable (Object String)
-  }
+type Config
+  = { atScopes :: Nullable (Object String)
+    , knownProperties :: Nullable (Array String)
+    , variables :: Nullable (Object String)
+    }
 
 hacss :: Fn2 Code (Nullable Config) CSS
-hacss = mkFn2 \code nullableConfig ->
-  let
-    emptyConfig = { atScopes: null, knownProperties: null, variables: null }
-    config = fromMaybe emptyConfig $ Null.toMaybe nullableConfig
-    atScopes = fromMaybe mempty $ Null.toMaybe config.atScopes
-    knownProperties = fromMaybe mempty $ Null.toMaybe config.knownProperties
-    variables = fromMaybe mempty $ Null.toMaybe config.variables
-  in
-    mkCSS
-      { atScopes: foldlWithIndex (\i b a -> b # Map.insert (AtScope i) a) Map.empty atScopes
-      , knownProperties: Property <$> (nub $ _knownProperties <> knownProperties)
-      , variables: foldlWithIndex (\i b a -> b # Map.insert (Variable i) a) Map.empty variables
-      }
-      code
+hacss =
+  mkFn2 \code nullableConfig ->
+    let
+      emptyConfig = { atScopes: null, knownProperties: null, variables: null }
+
+      config = fromMaybe emptyConfig $ Null.toMaybe nullableConfig
+
+      atScopes = fromMaybe mempty $ Null.toMaybe config.atScopes
+
+      knownProperties = fromMaybe mempty $ Null.toMaybe config.knownProperties
+
+      variables = fromMaybe mempty $ Null.toMaybe config.variables
+    in
+      mkCSS
+        { atScopes: foldlWithIndex (\i b a -> b # Map.insert (AtScope i) a) Map.empty atScopes
+        , knownProperties: Property <$> (nub $ _knownProperties <> knownProperties)
+        , variables: foldlWithIndex (\i b a -> b # Map.insert (Variable i) a) Map.empty variables
+        }
+        code
 
 foreign import _knownProperties :: Array String
